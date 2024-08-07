fd12e395f55da8d89823d4334a7402307bc0a28e
Jarred Brown <jarredbrownmusic@gmail.com>
2024-08-07 07:55:52 -0400

    add floorplan map to panoramic viewer

--- scripts/panoramic-viewer.js
+++ scripts/panoramic-viewer.js
@@ -1,24 +1,28 @@
 import { Viewer } from '@photo-sphere-viewer/core';
 
 export const panoramicViewer = () => {
-    const panoramaImagePath = new URL('../assets/images/Panorama.jpg', import.meta.url);
-    const panoramaImage2Path = new URL('../assets/images/Panorama2.jpg', import.meta.url);
-    const panoramaImage3Path = new URL('../assets/images/Panorama3.jpg', import.meta.url);
-    const panoramaImage4Path = new URL('../assets/images/Panorama4.jpg', import.meta.url);
-    const nextButton = document.getElementById('panoramic-right-arrow');
-    const dotsContainer = document.getElementById('panoramicDots'); // Assuming this is the ID for the dots container
+    const foyer = new URL('../assets/images/Mikes Cottage/01 FOYER.jpg', import.meta.url);
+    const diningRoom = new URL('../assets/images/Mikes Cottage/02 DINING ROOM.jpg', import.meta.url);
+    const greatRoom = new URL('../assets/images/Mikes Cottage/03 GREAT ROOM.jpg', import.meta.url);
+    const kitchen = new URL('../assets/images/Mikes Cottage/04 KITCHEN.jpg', import.meta.url);
+    const kitchen2 = new URL('../assets/images/Mikes Cottage/05 KITCHEN 02.jpg', import.meta.url);
+    const hallway = new URL('../assets/images/Mikes Cottage/06 HALLWAY.jpg', import.meta.url);
+    const masterBedroom = new URL('../assets/images/Mikes Cottage/07 MASTER BEDROOM.jpg', import.meta.url);
+    const ensuite = new URL('../assets/images/Mikes Cottage/08 ENSUITE.jpg', import.meta.url);
+
+    const dotsContainer = document.getElementById('panoramicDots');
 
     let count = 0;
 
     const images = [
-        { image: panoramaImagePath.href, room: 'Front Entrance' },
-        { image: panoramaImage2Path.href, room: 'Dining Room' },
-        { image: panoramaImage3Path.href, room: 'Living Room' },
-        { image: panoramaImage4Path.href, room: 'Kitchen' },
-        { image: panoramaImagePath.href, room: 'Front Entrance' },
-        { image: panoramaImage2Path.href, room: 'Dining Room' },
-        { image: panoramaImage3Path.href, room: 'Living Room' },
-        { image: panoramaImage4Path.href, room: 'Kitchen' }
+        { image: foyer.href, room: 'Foyer', imageFP: '../assets/images/Mikes Cottage/01 FOYER.pdf'},
+        { image: diningRoom.href, room: 'Dining Room', imageFP: '../assets/images/Mikes Cottage/02 DINING ROOM.pdf' },
+        { image: greatRoom.href, room: 'Great Room', imageFP: '../assets/images/Mikes Cottage/03 GREAT ROOM.pdf' },
+        { image: kitchen.href, room: 'Kitchen', imageFP: '../assets/images/Mikes Cottage/04 KITCHEN.pdf' },
+        { image: kitchen2.href, room: 'Kitchen 02', imageFP: '../assets/images/Mikes Cottage/05 KITCHEN 02.pdf' },
+        { image: hallway.href, room: 'Hallway', imageFP: '../assets/images/Mikes Cottage/06 HALLWAY.pdf' },
+        { image: masterBedroom.href, room: 'Master Bedroom', imageFP: '../assets/images/Mikes Cottage/07 MASTER BEDROOM.pdf' },
+        { image: ensuite.href, room: 'Ensuite', imageFP: '../assets/images/Mikes Cottage/08 ENSUITE.pdf' }
     ];
 
     // Create dots with custom tooltip
@@ -62,9 +66,16 @@ export const panoramicViewer = () => {
     const viewerContainer = document.querySelector('#viewer');
     viewerContainer.innerHTML = ''; // Clear the viewer container
 
+    // Create and append the floor plan image
+    const FPMap = document.createElement('img');
+    FPMap.src = images[count].imageFP ? images[count].imageFP : '';
+    FPMap.classList.add('fp-map');
+    viewerContainer.appendChild(FPMap);
+
     // Create and append the left arrow
     const leftArrow = document.createElement('span');
     leftArrow.id = 'panoramic-left-arrow';
+    leftArrow.classList.add('panoramic-arrow');
     leftArrow.textContent = ' < ';
     leftArrow.addEventListener('click', prevImage);
     viewerContainer.appendChild(leftArrow);
@@ -72,28 +83,34 @@ export const panoramicViewer = () => {
     // Create and append the right arrow
     const rightArrow = document.createElement('span');
     rightArrow.id = 'panoramic-right-arrow';
+    rightArrow.classList.add('panoramic-arrow');
     rightArrow.textContent = ' > ';
     rightArrow.addEventListener('click', nextImage);
     viewerContainer.appendChild(rightArrow);
+
+    if (images.length <= 1) {
+        leftArrow.style.display = 'none';
+        rightArrow.style.display = 'none';
+    }
 
     // Create dots container within the viewer container
     const dotsContainer = document.createElement('div');
     dotsContainer.id = 'panoramicDots';
     viewerContainer.appendChild(dotsContainer);
 
-        // Create dots with custom tooltip
-        for (let i = 0; i < images.length; i++) {
-            const dot = document.createElement('div');
-            dot.classList.add('dot');
+    // Create dots with custom tooltip
+    for (let i = 0; i < images.length; i++) {
+        const dot = document.createElement('div');
+        dot.classList.add('dot');
 
-            const tooltipText = document.createElement('span');
-            tooltipText.classList.add('tooltip-text');
-            tooltipText.textContent = images[i].room;
+        const tooltipText = document.createElement('span');
+        tooltipText.classList.add('tooltip-text');
+        tooltipText.textContent = images[i].room;
 
-            dot.appendChild(tooltipText);
-            dot.addEventListener('click', () => showImage(i));
-            dotsContainer.appendChild(dot);
-        }
+        dot.appendChild(tooltipText);
+        dot.addEventListener('click', () => showImage(i));
+        dotsContainer.appendChild(dot);
+    }
 
     // Update dots active state
     const dots = dotsContainer.getElementsByClassName('dot');
@@ -114,15 +131,12 @@ export const panoramicViewer = () => {
             caption: images[count].room,
             defaultZoomLvl: 0,
             navbar: [
-                'caption',
                 'zoom',
+                'caption',
                 'move',
                 'fullscreen',
             ],
         });
     }
-
-    // Attach the initial event listener to the next button
-    nextButton.addEventListener('click', nextImage);
     updateViewer();
 };
